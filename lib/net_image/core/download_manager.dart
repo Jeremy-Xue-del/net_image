636d9989cc2c716e356a9074e5ada3110514797b
Jeremy <xuejinming@snto.com>
2025-11-17 09:33:51 +0800

    整理项目

--- lib/net_image/core/download_manager.dart
+++ lib/net_image/core/download_manager.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 import 'dart:typed_data';
 import 'package:flutter/cupertino.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
 import '../model/download_model.dart';
 import '../utils/image_utils.dart';
@@ -40,112 +40,6 @@ class DefaultImageDownloader implements ImageDownloader {
 
     return taskId;
   }
-
-  /// 执行下载任务
-  // Future<void> _performDownload(_DownloadTaskInfo taskInfo) async {
-  //   try {
-  //     final url = taskInfo.model.url;
-  //     if (url == null) {
-  //       final errorModel = DownloadModel(
-  //         url: taskInfo.model.url,
-  //         path: taskInfo.model.path,
-  //         progress: 0.0,
-  //         status: DownloadStatus.failed,
-  //       );
-  //       final callback = taskInfo.callback.target;
-  //       if (callback != null) {
-  //         callback.onError(taskInfo.taskId, errorModel);
-  //       }
-  //       _tasks.remove(taskInfo.taskId);
-  //       return;
-  //     }
-  //
-  //     // 发送HTTP请求
-  //     final client = http.Client();
-  //     final request = http.Request('GET', Uri.parse(url));
-  //     final response = await client.send(request);
-  //
-  //     if (response.statusCode != 200) {
-  //       client.close();
-  //       final errorModel = DownloadModel(
-  //         url: taskInfo.model.url,
-  //         path: taskInfo.model.path,
-  //         progress: 0.0,
-  //         status: DownloadStatus.failed,
-  //       );
-  //       final callback = taskInfo.callback.target;
-  //       if (callback != null) {
-  //         callback.onError(taskInfo.taskId, errorModel);
-  //       }
-  //       _tasks.remove(taskInfo.taskId);
-  //       return;
-  //     }
-  //
-  //     // 读取数据并更新进度
-  //     final data = <int>[];
-  //     final totalBytes = response.contentLength ?? 1;
-  //     var receivedBytes = 0;
-  //
-  //     await for (final chunk in response.stream) {
-  //       data.addAll(chunk);
-  //       receivedBytes += chunk.length;
-  //
-  //       // 计算进度并通知
-  //       final progress = totalBytes > 0 ? receivedBytes / totalBytes : 0.0;
-  //       final progressModel = DownloadModel(
-  //         url: taskInfo.model.url,
-  //         path: taskInfo.model.path,
-  //         progress: progress,
-  //         status: DownloadStatus.downing,
-  //       );
-  //       final callback = taskInfo.callback.target;
-  //       if (callback != null) {
-  //         callback.onProgress(taskInfo.taskId, progressModel);
-  //       }
-  //     }
-  //
-  //     client.close();
-  //
-  //     // 转换为Uint8List
-  //     final imageData = Uint8List.fromList(data);
-  //
-  //     // 保存到文件
-  //     final filePath = taskInfo.model.path ?? await _getDefaultFilePath(url);
-  //     final file = File(filePath);
-  //     await file.create(recursive: true);
-  //     await file.writeAsBytes(imageData);
-  //
-  //     // 通知完成
-  //     final completeModel = DownloadModel(
-  //       url: taskInfo.model.url,
-  //       path: filePath,
-  //       progress: 1.0,
-  //       status: DownloadStatus.finish,
-  //     );
-  //     final callback = taskInfo.callback.target;
-  //     if (callback != null) {
-  //       callback.onComplete(taskInfo.taskId, completeModel);
-  //     }
-  //
-  //     // 从任务列表中移除
-  //     _tasks.remove(taskInfo.taskId);
-  //   } catch (e) {
-  //     // 通知错误
-  //     final errorModel = DownloadModel(
-  //       url: taskInfo.model.url,
-  //       path: taskInfo.model.path,
-  //       progress: 0.0,
-  //       status: DownloadStatus.failed,
-  //     );
-  //     final callback = taskInfo.callback.target;
-  //     if (callback != null) {
-  //       callback.onError(taskInfo.taskId, errorModel);
-  //     }
-  //
-  //     // 从任务列表中移除
-  //     _tasks.remove(taskInfo.taskId);
-  //   }
-  // }
   /// 执行下载任务
   Future<void> _performDownload(_DownloadTaskInfo taskInfo) async {
     try {
@@ -167,8 +61,8 @@ class DefaultImageDownloader implements ImageDownloader {
       }
 
       // 发送HTTP请求
-      final client = http.Client();
-      final request = http.Request('GET', Uri.parse(url));
+      final client = Client();
+      final request = Request('GET', Uri.parse(url));
       final response = await client.send(request);
 
       debugPrint('收到响应，状态码: ${response.statusCode}'); // 添加调试日志
